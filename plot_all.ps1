8834f0bc2e4f37be3f11411f22d8cee324c018d0
Nan Ma <nanma@microsoft.com>
2020-11-07 20:34:58 -0800

    outer no tracking

---
+++ plot_all.ps1
@@ -0,0 +1,6 @@
+wolframscript.exe .\rotating_rolling.wls outer
+wolframscript.exe .\rotating_rolling.wls outer edges nodes
+wolframscript.exe .\rotating_rolling.wls outer nodes
+wolframscript.exe .\rotating_rolling.wls inner
+wolframscript.exe .\rotating_rolling.wls inner edges nodes
+wolframscript.exe .\rotating_rolling.wls inner nodes
