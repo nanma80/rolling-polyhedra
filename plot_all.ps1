66b0cee868ed019ea5ecf75773e530b80483de6e
Nan Ma <nanma@microsoft.com>
2020-11-08 18:18:24 -0800

    rename things

--- plot_all.ps1
+++ plot_all.ps1
@@ -1,6 +1,12 @@
-wolframscript.exe .\rotating_rolling.wls outer
-wolframscript.exe .\rotating_rolling.wls outer edges nodes
-wolframscript.exe .\rotating_rolling.wls outer nodes
-wolframscript.exe .\rotating_rolling.wls inner
-wolframscript.exe .\rotating_rolling.wls inner edges nodes
-wolframscript.exe .\rotating_rolling.wls inner nodes
+wolframscript.exe .\octa_tetra.wls outer
+wolframscript.exe .\octa_tetra.wls outer edges nodes
+# wolframscript.exe .\octa_tetra.wls outer nodes
+wolframscript.exe .\octa_tetra.wls inner
+wolframscript.exe .\octa_tetra.wls inner edges nodes
+wolframscript.exe .\octa_tetra.wls inner nodes
+wolframscript.exe .\icosa_tetra.wls outer
+wolframscript.exe .\icosa_tetra.wls outer edges nodes
+wolframscript.exe .\icosa_tetra.wls outer nodes
+wolframscript.exe .\icosa_tetra.wls inner
+wolframscript.exe .\icosa_tetra.wls inner edges nodes
+wolframscript.exe .\icosa_tetra.wls inner nodes
