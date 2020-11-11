de5c14ff498f812460c50d69ff0811a8f8d5c3af
Nan Ma <nanma@microsoft.com>
2020-11-11 11:43:45 -0800

    smaller balls for inner

--- plot_all.ps1
+++ plot_all.ps1
@@ -7,12 +7,13 @@
 # wolframscript.exe .\icosa_tetra.wls outer
 # wolframscript.exe .\icosa_tetra.wls outer edges nodes
 # wolframscript.exe .\icosa_tetra.wls outer nodes
-# wolframscript.exe .\icosa_tetra.wls inner
-# wolframscript.exe .\icosa_tetra.wls inner edges nodes
-# wolframscript.exe .\icosa_tetra.wls inner nodes
-wolframscript.exe .\icosa_octa.wls outer
-wolframscript.exe .\icosa_octa.wls outer nodes
-wolframscript.exe .\icosa_octa.wls outer edges nodes
-wolframscript.exe .\icosa_octa.wls inner
-wolframscript.exe .\icosa_octa.wls inner nodes
-wolframscript.exe .\icosa_octa.wls inner edges nodes
+wolframscript.exe .\icosa_tetra.wls inner faces
+wolframscript.exe .\icosa_tetra.wls inner
+wolframscript.exe .\icosa_tetra.wls inner edges nodes
+wolframscript.exe .\icosa_tetra.wls inner nodes
+# wolframscript.exe .\icosa_octa.wls outer
+# wolframscript.exe .\icosa_octa.wls outer nodes
+# wolframscript.exe .\icosa_octa.wls outer edges nodes
+# wolframscript.exe .\icosa_octa.wls inner
+# wolframscript.exe .\icosa_octa.wls inner nodes
+# wolframscript.exe .\icosa_octa.wls inner edges nodes
