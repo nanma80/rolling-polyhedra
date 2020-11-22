43ed8e290afd98ae5033e960b14d84e4baa2ee8d
Nan Ma <nanma@microsoft.com>
2020-11-21 16:00:02 -0800

    720

--- plot_all.ps1
+++ plot_all.ps1
@@ -4,17 +4,17 @@
 # wolframscript.exe .\octa_tetra.wls inner
 # wolframscript.exe .\octa_tetra.wls inner edges nodes
 # wolframscript.exe .\octa_tetra.wls inner nodes
-wolframscript.exe .\icosa_tetra.wls outer faces
+# wolframscript.exe .\icosa_tetra.wls outer faces
 # wolframscript.exe .\icosa_tetra.wls outer
 # wolframscript.exe .\icosa_tetra.wls outer edges nodes
 # wolframscript.exe .\icosa_tetra.wls outer nodes
 # wolframscript.exe .\icosa_tetra.wls inner faces
 # wolframscript.exe .\icosa_tetra.wls inner
-# wolframscript.exe .\icosa_tetra.wls inner edges nodes
-# wolframscript.exe .\icosa_tetra.wls inner nodes
-# wolframscript.exe .\icosa_octa.wls outer
-# wolframscript.exe .\icosa_octa.wls outer nodes
-# wolframscript.exe .\icosa_octa.wls outer edges nodes
-# wolframscript.exe .\icosa_octa.wls inner
-# wolframscript.exe .\icosa_octa.wls inner nodes
-# wolframscript.exe .\icosa_octa.wls inner edges nodes
+wolframscript.exe .\icosa_tetra.wls inner edges nodes
+wolframscript.exe .\icosa_tetra.wls inner nodes
+wolframscript.exe .\icosa_octa.wls outer
+wolframscript.exe .\icosa_octa.wls outer nodes
+wolframscript.exe .\icosa_octa.wls outer edges nodes
+wolframscript.exe .\icosa_octa.wls inner
+wolframscript.exe .\icosa_octa.wls inner nodes
+wolframscript.exe .\icosa_octa.wls inner edges nodes
