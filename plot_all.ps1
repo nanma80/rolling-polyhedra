92ed47b12b1ee9c8d96e514611cabcef9d7a998e
Nan Ma <nanma@microsoft.com>
2020-12-10 22:59:05 -0800

    dodeca great dodeca

--- plot_all.ps1
+++ plot_all.ps1
@@ -10,11 +10,19 @@
 # wolframscript.exe .\icosa_tetra.wls outer nodes
 # wolframscript.exe .\icosa_tetra.wls inner faces
 # wolframscript.exe .\icosa_tetra.wls inner
-wolframscript.exe .\icosa_tetra.wls inner edges nodes
-wolframscript.exe .\icosa_tetra.wls inner nodes
-wolframscript.exe .\icosa_octa.wls outer
-wolframscript.exe .\icosa_octa.wls outer nodes
-wolframscript.exe .\icosa_octa.wls outer edges nodes
-wolframscript.exe .\icosa_octa.wls inner
-wolframscript.exe .\icosa_octa.wls inner nodes
-wolframscript.exe .\icosa_octa.wls inner edges nodes
+# wolframscript.exe .\icosa_tetra.wls inner edges nodes
+# wolframscript.exe .\icosa_tetra.wls inner nodes
+# wolframscript.exe .\icosa_octa.wls outer
+# wolframscript.exe .\icosa_octa.wls outer nodes
+# wolframscript.exe .\icosa_octa.wls outer edges nodes
+# wolframscript.exe .\icosa_octa.wls inner
+# wolframscript.exe .\icosa_octa.wls inner nodes
+# wolframscript.exe .\icosa_octa.wls inner edges nodes
+wolframscript.exe .\dodeca_great.wls outer faces
+wolframscript.exe .\dodeca_great.wls outer
+wolframscript.exe .\dodeca_great.wls outer edges nodes
+wolframscript.exe .\dodeca_great.wls outer nodes
+wolframscript.exe .\dodeca_great.wls inner faces
+wolframscript.exe .\dodeca_great.wls inner
+wolframscript.exe .\dodeca_great.wls inner edges nodes
+wolframscript.exe .\dodeca_great.wls inner nodes
